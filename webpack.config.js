219d7f409747f789188a2756641a867a293ca153
arsalyou <arsalyou@users.noreply.github.com>
2022-06-25 09:33:38 +0500

    stable build

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = function(_env, argv) {
   const isDevelopment = !isProduction;
 
   return {
-    devtool: isDevelopment && "cheap-module-source-map",
+    devtool: isDevelopment && "eval-source-map",
     entry: "./src/index.js",
     output: {
       path: path.resolve(__dirname, './public'),
